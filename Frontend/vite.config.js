0f6932e82a71e296ba1622110228fc30c3e0cee2
Akash Yadav <136945099+YdvAkash@users.noreply.github.com>
2025-01-25 16:33:58 +0530

    vite.config.js Updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://localhost:5000',
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_REACT_APP_BACKEND_BASEURL, // Use process.env properly
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   plugins: [react()],
